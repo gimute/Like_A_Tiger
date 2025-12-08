f5af08fe6827d7a29503eb89c8b0e96016f26152
gimte <y4k2t36s2416@gmail.com>
2025-12-08 11:32:29 +0900

    改行機能追加

--- Game/UI/UILayout.h
+++ Game/UI/UILayout.h
@@ -8,12 +8,16 @@ class UILayout
 private:
 	//並べるUI
 	std::vector<UICanvas::RefUIBasePtr> m_uiList;
-
 	//ずらす量ベクトル
 	Vector3 m_offsetPosition;
-
 	//最初のUIの座標
 	Vector3 m_position;
+
+
+	//いくつUIが登録されるごとに改行するか、0なら改行しない
+	int m_newLineNum = 0;
+	//改行でずらす量ベクトル
+	Vector3 m_newLineOffsetPos;
 
 public:
 	//座標の更新
@@ -25,11 +29,29 @@ public:
 		m_uiList.push_back(ui);
 	}
 
+	//UIを取り除く
+	void RemoveUI(UICanvas::RefUIBasePtr ui)
+	{
+		auto newEnd = std::remove(m_uiList.begin(), m_uiList.end(), ui);
+		m_uiList.erase(newEnd, m_uiList.end());
+	}
+
 	//ずらすベクトルを設定
 	void SetOffset(Vector3 offset)
 	{
 		m_offsetPosition = offset;
 	}
 
+	//改行する際にずらすベクトル
+	void SetNewLineOffset(Vector3 offset)
+	{
+		m_newLineOffsetPos = offset;
+	}
+
+	//UIいくつごとに改行するか設定、0に設定すると改行しない
+	void SetNewLineNum(int newLineNum)
+	{
+		m_newLineNum = newLineNum;
+	}
 };
 
