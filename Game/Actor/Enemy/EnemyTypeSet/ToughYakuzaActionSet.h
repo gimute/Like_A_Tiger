867fdda707e8a799a7a22130c7713a4a4c4b1c01
takeda haruse <haruse010655@gmail.com>
2026-01-28 00:12:29 +0900

    とりあえずモデル作成、アニメ～ジョンはバックステップまで導入

--- Game/Actor/Enemy/EnemyTypeSet/ToughYakuzaActionSet.h
+++ Game/Actor/Enemy/EnemyTypeSet/ToughYakuzaActionSet.h
@@ -9,7 +9,16 @@ public:
 
 	ToughYakuzaActionSet() : IYakuzaTypeSet(en_campEnemy)
 	{
+		
 
+		m_modelFilePath = "Assets/modelData/Character/Adam/Adam.tkm";
+
+		m_animationDataList.push_back({ "Assets/modelData/Character/Adam/Animation/Idle.tka",true });
+		m_animationDataList.push_back({ "Assets/modelData/Character/Adam/Animation/Walking.tka",true });
+		m_animationDataList.push_back({ "Assets/modelData/Character/Adam/Animation/Running.tka",true });
+		m_animationDataList.push_back({ "Assets/modelData/Character/Adam/Animation/Fighting_Idle.tka",true });
+		m_animationDataList.push_back({ "Assets/modelData/Character/Adam/Animation/ForwardStep.tka",true });
+		m_animationDataList.push_back({ "Assets/modelData/Character/Adam/Animation/BackStep.tka",true });
 	}
 
 	void InitStateMachineParam(YakuzaCharacter& useCharacter, YakuzaStateMachine& useStateMachine) override
