4a000fbb6f35da873fe69461985fcb9f0c5c57cb
takeda haruse <haruse010655@gmail.com>
2026-01-28 11:05:22 +0900

    アニメーションを死亡モーションまで入れた、格闘モーションは1つ入れた

--- Game/Actor/Enemy/EnemyTypeSet/ToughYakuzaActionSet.cpp
+++ Game/Actor/Enemy/EnemyTypeSet/ToughYakuzaActionSet.cpp
@@ -1,2 +1,4 @@
 #include "stdafx.h"
 #include "ToughYakuzaActionSet.h"
+
+TypeSetAutoRegister<ToughYakuzaActionSet> ToughYakuzaActionSet::typeSet{ EnemyYakuzaType::en_toughYakuza };
--- Game/Actor/Enemy/EnemyTypeSet/ToughYakuzaActionSet.h
+++ Game/Actor/Enemy/EnemyTypeSet/ToughYakuzaActionSet.h
@@ -7,6 +7,11 @@ class ToughYakuzaActionSet : public IYakuzaTypeSet
 	appState(ToughYakuzaActionSet)
 public:
 
+	enum ToughYakuzaAnimation
+	{
+		en_hookPunch_1_R = YakuzaAnimation::en_num,
+	};
+
 	ToughYakuzaActionSet() : IYakuzaTypeSet(en_campEnemy)
 	{
 		
@@ -19,6 +24,26 @@ public:
 		m_animationDataList.push_back({ "Assets/modelData/Character/Adam/Animation/Fighting_Idle.tka",true });
 		m_animationDataList.push_back({ "Assets/modelData/Character/Adam/Animation/ForwardStep.tka",true });
 		m_animationDataList.push_back({ "Assets/modelData/Character/Adam/Animation/BackStep.tka",true });
+		m_animationDataList.push_back({ "Assets/modelData/Character/Adam/Animation/SideStep_Right.tka",true });
+		m_animationDataList.push_back({ "Assets/modelData/Character/Adam/Animation/SideStep_Left.tka",true });
+		m_animationDataList.push_back({ "Assets/modelData/Character/Adam/Animation/SideStep_Left.tka",true });//この敵は回避動作をしないため、仮のモーションを入れておく
+		m_animationDataList.push_back({ "Assets/modelData/Character/Adam/Animation/SideStep_Left.tka",true });//この敵は回避動作をしないため、仮のモーションを入れておく
+		m_animationDataList.push_back({ "Assets/modelData/Character/Adam/Animation/SideStep_Left.tka",true });//この敵は回避動作をしないため、仮のモーションを入れておく
+		m_animationDataList.push_back({ "Assets/modelData/Character/Adam/Animation/SideStep_Left.tka",true });//この敵は回避動作をしないため、仮のモーションを入れておく
+		m_animationDataList.push_back({ "Assets/modelData/Character/Adam/Animation/Guard_Idle_A.tka",true });
+		m_animationDataList.push_back({ "Assets/modelData/Character/Adam/Animation/Guard_Idle_A.tka",true });//この敵は掴み動作をしないため、仮のモーションを入れておく
+		m_animationDataList.push_back({ "Assets/modelData/Character/Adam/Animation/Guard_Idle_A.tka",true });//この敵は掴み動作をしないため、仮のモーションを入れておく
+		m_animationDataList.push_back({ "Assets/modelData/Character/Adam/Animation/Guard_Idle_A.tka",true });//この敵は掴み動作をしないため、仮のモーションを入れておく
+		m_animationDataList.push_back({ "Assets/modelData/Character/Adam/Animation/Guard_Idle_A.tka",true });//この敵は掴み動作をしないため、仮のモーションを入れておく
+		m_animationDataList.push_back({ "Assets/modelData/Character/Adam/Animation/Guard_Idle_A.tka",true });//この敵は掴み動作をしないため、仮のモーションを入れておく
+		m_animationDataList.push_back({ "Assets/modelData/Character/Adam/Animation/Grab_Idle_B.tka",true });
+		m_animationDataList.push_back({ "Assets/modelData/Character/Adam/Animation/Grab_Throw_B.tka",false});
+		m_animationDataList.push_back({ "Assets/modelData/Character/Adam/Animation/Grab_Break_B.tka",false });
+		m_animationDataList.push_back({ "Assets/modelData/Character/Adam/Animation/Grab_Atk_B.tka",false });
+		m_animationDataList.push_back({ "Assets/modelData/Character/Adam/Animation/BodyHit.tka",false });
+		m_animationDataList.push_back({ "Assets/modelData/Character/Adam/Animation/BackDeath_E.tka",false });
+		m_animationDataList.push_back({ "Assets/modelData/Character/Adam/Animation/HookPunch_R.tka",false });//ここから戦闘モーション
+
 	}
 
 	void InitStateMachineParam(YakuzaCharacter& useCharacter, YakuzaStateMachine& useStateMachine) override
@@ -32,5 +57,7 @@ public:
 	{
 		//ここに攻撃ステート追加処理を記述
 	}
+private:
+	static TypeSetAutoRegister<ToughYakuzaActionSet> typeSet;
 };
 
