dd8808d60ddcaa9b1f636de20bdb618d431c76a3
takeda haruse <haruse010655@gmail.com>
2026-02-27 23:10:15 +0900

    格闘以外のモーションを作成

--- Game/Actor/Enemy/EnemyTypeSet/HirotaYakuzaActionSet.h
+++ Game/Actor/Enemy/EnemyTypeSet/HirotaYakuzaActionSet.h
@@ -22,6 +22,29 @@ public:
 		m_modelFilePath = "C:Assets/modelData/Character/Crypto/Crypto.tkm";
 
 		m_animationDataList.push_back({ "Assets/modelData/Character/Crypto/Animation/Idle.tka",true });
+		m_animationDataList.push_back({ "Assets/modelData/Character/Crypto/Animation/Walking.tka",true });
+		m_animationDataList.push_back({ "Assets/modelData/Character/Crypto/Animation/Running.tka",true });
+		m_animationDataList.push_back({ "Assets/modelData/Character/Crypto/Animation/Fighting_Idle.tka",true });
+		m_animationDataList.push_back({ "Assets/modelData/Character/Crypto/Animation/ForwardStep.tka",true });
+		m_animationDataList.push_back({ "Assets/modelData/Character/Crypto/Animation/BackStep.tka",true });
+		m_animationDataList.push_back({ "Assets/modelData/Character/Crypto/Animation/SideStep_Right.tka",true });
+		m_animationDataList.push_back({ "Assets/modelData/Character/Crypto/Animation/SideStep_Left.tka",true });
+		m_animationDataList.push_back({ "Assets/modelData/Character/Crypto/Animation/Idle.tka",true });//この敵は回避動作をしないため、仮のモーションを入れておく
+		m_animationDataList.push_back({ "Assets/modelData/Character/Crypto/Animation/Idle.tka",true });//この敵は回避動作をしないため、仮のモーションを入れておく
+		m_animationDataList.push_back({ "Assets/modelData/Character/Crypto/Animation/Idle.tka",true });//この敵は回避動作をしないため、仮のモーションを入れておく
+		m_animationDataList.push_back({ "Assets/modelData/Character/Crypto/Animation/Idle.tka",true });//この敵は回避動作をしないため、仮のモーションを入れておく
+		m_animationDataList.push_back({ "Assets/modelData/Character/Crypto/Animation/Idle.tka",true });//この敵は防御動作をしないため、仮のモーションを入れておく
+		m_animationDataList.push_back({ "Assets/modelData/Character/Crypto/Animation/Idle.tka",true });//この敵は掴み動作をしないため、仮のモーションを入れておく
+		m_animationDataList.push_back({ "Assets/modelData/Character/Crypto/Animation/Idle.tka",true });//この敵は掴み動作をしないため、仮のモーションを入れておく
+		m_animationDataList.push_back({ "Assets/modelData/Character/Crypto/Animation/Idle.tka",true });//この敵は掴み動作をしないため、仮のモーションを入れておく
+		m_animationDataList.push_back({ "Assets/modelData/Character/Crypto/Animation/Idle.tka",true });//この敵は掴み動作をしないため、仮のモーションを入れておく
+		m_animationDataList.push_back({ "Assets/modelData/Character/Crypto/Animation/Idle.tka",true });//この敵は掴み動作をしないため、仮のモーションを入れておく
+		m_animationDataList.push_back({ "Assets/modelData/Character/Crypto/Animation/Idle.tka",true });//掴まれは一旦待ち
+		m_animationDataList.push_back({ "Assets/modelData/Character/Crypto/Animation/Idle.tka",true });//掴まれは一旦待ち
+		m_animationDataList.push_back({ "Assets/modelData/Character/Crypto/Animation/Idle.tka",true });//掴まれは一旦待ち
+		m_animationDataList.push_back({ "Assets/modelData/Character/Crypto/Animation/Idle.tka",true });//掴まれは一旦待ち
+		m_animationDataList.push_back({ "Assets/modelData/Character/Crypto/Animation/BodyHit.tka",false });
+		m_animationDataList.push_back({ "Assets/modelData/Character/Crypto/Animation/BackDeath_E.tka",false });
 	}
 
 	void InitStateMachineParam(YakuzaCharacter& useCharacter, YakuzaStateMachine& useStateMachine) override
